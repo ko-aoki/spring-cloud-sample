6b5d69153ba9ca0214b241b77e9cd202321b72a7
ko-aoki <ko.aoki1223@gmail.com>
2020-11-01 14:24:52 +0900

    Spring Cloug Gatewayにoauth2設定追加

---
+++ cloud-gateway/src/main/java/com/example/cloudgateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.cloudgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2Login(withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+
+}
